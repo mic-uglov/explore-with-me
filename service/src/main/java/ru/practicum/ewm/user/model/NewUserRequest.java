0dd34a5c7e20309c6c63c343f93f2c1fa78854a2
uglov <mic-uglov@yandex.ru>
2023-03-08 13:02:55 +0300

    fix: NotBlank with Email

--- service/src/main/java/ru/practicum/ewm/user/model/NewUserRequest.java
+++ service/src/main/java/ru/practicum/ewm/user/model/NewUserRequest.java
@@ -5,12 +5,11 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class NewUserRequest {
-    @NotNull
+    @NotBlank
     @Email
     private String email;
 
