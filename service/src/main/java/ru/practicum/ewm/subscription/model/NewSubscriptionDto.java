05a43c9e7eaa30241331da7dabcc282fbe0e78e6
uglov <mic-uglov@yandex.ru>
2023-03-17 11:17:36 +0300

    fix: after review @Size for name in subscription dtos

--- service/src/main/java/ru/practicum/ewm/subscription/model/NewSubscriptionDto.java
+++ service/src/main/java/ru/practicum/ewm/subscription/model/NewSubscriptionDto.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class NewSubscriptionDto {
     @NotBlank
+    @Size(max = 64)
     private String name;
     private List<Long> initiators;
 }
--- service/src/main/java/ru/practicum/ewm/subscription/model/UpdateSubscriptionDto.java
+++ service/src/main/java/ru/practicum/ewm/subscription/model/UpdateSubscriptionDto.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import ru.practicum.ewm.validation.NotBlankOrNull;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 public class UpdateSubscriptionDto {
     @NotBlankOrNull
+    @Size(max = 64)
     private String name;
     private List<Long> initiators;
 }
